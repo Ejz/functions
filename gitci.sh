740ac0c73bcb1c945eda93290d6f539ff0cf570c
Ejz <ejz@ya.ru>
2017-08-17 23:21:23 +0300

    _

--- gitci.sh
+++ gitci.sh
@@ -5,8 +5,13 @@ branch="$TRAVIS_BRANCH"
 
 cd "`dirname "$0"`"
 
-# Pull requests and commits just build, no deploy!!!
-if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "master" ]; then
+function inc_ver() {
+    script='NF==1{print ++$NF};NF>1{if(length($NF+1)>length($NF))$(NF-1)++;$NF=sprintf("%0*d",length($NF),($NF+1)%(10^length($NF)));print}'
+    echo "$1" | awk -F. -v OFS=. "$script"
+}
+
+# Pull requests and commits to not master branch just build, no deploy!!!
+if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$branch" != "master" ]; then
     echo "SKIP!"
     exit 0
 fi
@@ -18,4 +23,16 @@ git remote set-url origin "$t"
 git status
 git add "$1"
 git commit -m "$2"
+tag=`git tag | grep -P '^v1\.\d+\.\d+' | tail -n 1`
+if [ -z "$tag" ]; then
+    ntag="v1.0.0"
+elif echo "$tag" | grep -qP -- '-RC\d+$'; then
+    ntag=`inc_ver "$tag"`
+else
+    ntag=`inc_ver "$tag"`
+    ntag="${ntag}-RC1"
+fi
+echo "Add ${ntag} tag!"
+git tag -a "$ntag" -m "$ntag"
 git push origin "$branch"
+git push --tags
